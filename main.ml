e8e714f0e394dbccc9d2ed5ab465df87711a7c7f
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2026-03-05 16:35:47 +0100

    eval with env and values

--- main.ml
+++ main.ml
@@ -3,6 +3,14 @@ module Option = struct
 
   let is_some_and f = function Some x -> f x | None -> false
   let and_then f = function Some x -> f x | None -> None
+  let get_or_else f = function Some x -> x | None -> f ()
+end
+
+module Result = struct
+  include Result
+
+  let and_then f = function Ok x -> f x | Error _ as e -> e
+  let get_or_else f = function Ok x -> x | Error u -> f u
 end
 
 module Char = struct
@@ -26,21 +34,37 @@ let eprintln fmt = Format.eprintf (fmt ^^ "@.")
 
 module Token = struct
   type t =
+    | Eof
+    (* items *)
     | Id of string
     | Num of string
+    (* delims *)
     | Lam
     | Dot
     | LParen
     | RParen
+    (* operators *)
     | Plus
     | Minus
     | Star
     | Slash
-    | Eof
+    | Eq
+    | EqEq
+    | Bang
+    | BangEq
+    | Gt
+    | GtEq
+    | Lt
+    | LtEq
   [@@deriving show { with_path = false }]
 end
 
-module Lexer = struct
+module Lexer : sig
+  type t
+
+  val init : string -> t
+  val next_token : t -> Token.t option
+end = struct
   type t = { source : string; mutable cursor : int }
 
   let init source = { source; cursor = 0 }
@@ -63,19 +87,32 @@ module Lexer = struct
     in
     loop ()
 
+  let matches lexer m =
+    if peek lexer |> Option.is_some_and (( = ) m) then
+      let _ = advance lexer in
+      true
+    else false
+
   let next_token lexer =
     let open Token in
     seek lexer Char.is_whitespace;
     advance lexer
     |> Option.map @@ function
+       (* delims *)
        | '\\' -> Lam
        | '.' -> Dot
        | '(' -> LParen
        | ')' -> RParen
+       (* operators *)
        | '+' -> Plus
        | '-' -> Minus
        | '*' -> Star
        | '/' -> Slash
+       | '=' -> if matches lexer '=' then EqEq else Eq
+       | '!' -> if matches lexer '=' then BangEq else Bang
+       | '>' -> if matches lexer '=' then GtEq else Gt
+       | '<' -> if matches lexer '=' then LtEq else Lt
+       (* items *)
        | c when c = '_' || Char.is_alpha c ->
            let start = lexer.cursor - 1 in
            seek lexer (fun c -> c = '_' || Char.is_alnum c);
@@ -100,17 +137,28 @@ module Ast = struct
   type expr =
     | Lit of int
     | Var of string
-    | Bin of expr * Token.t * expr
-    | Abs of string * expr
-    | App of expr * expr
+    | Bin of (expr * Token.t * expr)
+    | Abs of (string * expr)
+    | App of (expr * expr)
   [@@deriving show { with_path = false }]
 
   let show_expr_list = [%show: expr list]
 end
 
-module Parser = struct
+module Parser : sig
+  type t
+
+  exception Exn of string
+
+  val init : string -> t
+  val parse : string -> Ast.expr option
+end = struct
   (*
-    expr   := term ;
+    expr   := or ;
+    or     := and ('or' and)* ;
+    and    := cond ('and' cond)* ;
+    eq     := cmp (('==' | '!=') cmp)* ;
+    cmp    := term (('>' | '>=' | '<' | '<=') term)* ;
     term   := factor (('+' | '-') factor)* ;
     factor := app (('*' | '/') app)* ;
     app    := atom atom* ;
@@ -152,7 +200,35 @@ module Parser = struct
     advance parser;
     parser.current <> Token.Eof |> Bool.then_some (fun _ -> parse_expr parser)
 
-  and parse_expr parser = parse_term parser
+  and parse_expr parser = parse_eq parser
+
+  (** eq := cmp (('==' | '!=') cmp)* ; *)
+  and parse_eq parser =
+    let expr = parse_cmp parser in
+    let rec loop expr =
+      match parser.current with
+      | Token.EqEq | Token.BangEq ->
+          advance parser;
+          let op = parser.previous in
+          let rhs = parse_cmp parser in
+          loop (Ast.Bin (expr, op, rhs))
+      | _ -> expr
+    in
+    loop expr
+
+  (** cmp := term (('>' | '>=' | '<' | '<=') term)* ; *)
+  and parse_cmp parser =
+    let expr = parse_term parser in
+    let rec loop expr =
+      match parser.current with
+      | Token.Gt | Token.GtEq | Token.Lt | Token.LtEq ->
+          advance parser;
+          let op = parser.previous in
+          let rhs = parse_term parser in
+          loop (Ast.Bin (expr, op, rhs))
+      | _ -> expr
+    in
+    loop expr
 
   (** term := factor (('+' | '-') factor)* ; *)
   and parse_term parser =
@@ -209,20 +285,20 @@ module Parser = struct
 
   (** abs := '\' ID '.' expr ; *)
   and parse_abs parser =
-    let param = consume_ident parser "expected ident after '\\'" in
+    let input = consume_ident parser "expected ident after '\\'" in
     (* nested abs syntax sugar -> \x y.x == \x.\y.x
        abs := '\' ID+ '.' expr ; *)
-    let rec loop param =
+    let rec loop input =
       advance parser;
       match parser.previous with
-      | Token.Dot -> Ast.Abs (param, parse_expr parser)
-      | Token.Id name -> Ast.Abs (param, loop name)
+      | Token.Dot -> Ast.Abs (input, parse_expr parser)
+      | Token.Id name -> Ast.Abs (input, loop name)
       | _ -> error "expected '.' or ident"
     in
-    loop param
+    loop input
 end
 
-module Runtime = struct
+module Redex = struct
   open Ast
 
   exception Exn of string
@@ -257,25 +333,106 @@ module Runtime = struct
         | _ -> error "expected abs")
 end
 
-type eval_error = ParseError of string | RuntimeError of string
+module Runtime = struct
+  exception Exn of string
 
-let eval source : (Ast.expr option, eval_error) result =
-  try
-    match Parser.parse source with
-    | Some expr -> Ok (Some (Runtime.reduce expr))
-    | None -> Ok None
-  with
-  | Parser.Exn reason -> Error (ParseError reason)
-  | Runtime.Exn reason -> Error (RuntimeError reason)
+  let error reason = raise (Exn reason)
+
+  module Env = Map.Make (String)
+
+  module Value = struct
+    type t =
+      | Int of int
+      | Fun of (string * Ast.expr * t Env.t)
+      | Native of (string * (t -> t))
+
+    let rec show = function
+      | Int x -> string_of_int x
+      | Fun _ -> "<fn>"
+      | Native (name, _) -> format "<native fn '%s'>" name
+
+    let as_fun = function Fun (_ as f) -> f | _ -> error "expected fun"
+  end
+
+  open Ast
+  open Value
+
+  let rec eval' env = function
+    | Lit x -> Int x
+    | Var name ->
+        Env.find_opt name env
+        |> Option.get_or_else (fun _ -> error (format "unbound var %s" name))
+    | Abs (input, expr) -> Fun (input, expr, env)
+    | App (_ as app) -> eval_app env app
+    | Bin (_ as bin) -> eval_bin env bin
+
+  and eval_app env (lhs, rhs) =
+    let lhs = eval' env lhs in
+    let rhs = eval' env rhs in
+    match lhs with
+    | Native (_, f) -> f rhs
+    | Fun (input, expr, env') -> eval' (Env.add input rhs env') expr
+    | _ -> error "only functions are callable"
+
+  and eval_bin env (lhs, op, rhs) =
+    let lhs, rhs =
+      match (eval' env lhs, eval' env rhs) with
+      | Int x, Int y -> (x, y)
+      | _ -> error "can only add integers"
+    in
+    let res =
+      match op with
+      | Token.Plus -> lhs + rhs
+      | Token.Minus -> lhs - rhs
+      | Token.Star -> lhs * rhs
+      | Token.Slash -> lhs / rhs
+      | _ -> assert false
+    in
+    Int res
+
+  let add_native name f = Env.add name (Native (name, f))
+
+  let init_env () =
+    Env.empty
+    |> add_native "echo" @@ fun v ->
+       Value.show v |> print_endline;
+       Int 0
+
+  let eval = eval' (init_env ())
+end
+
+type interpret_error = Parse_error of string | Runtime_error of string
+
+let interpret source =
+  try Ok (Parser.parse source |> Option.map Runtime.eval) with
+  | Parser.Exn reason -> Error (Parse_error reason)
+  | Runtime.Exn reason -> Error (Runtime_error reason)
 
 let rec repl () =
   try
-    (match read_line () |> eval with
-    | Ok (Some expr) -> Ast.show_expr expr |> print_endline
-    | Error (ParseError reason) -> eprintln "parse error: %s" reason
-    | Error (RuntimeError reason) -> eprintln "runtime error: %s" reason
+    (match read_line () |> interpret with
+    | Ok (Some expr) -> Runtime.Value.show expr |> print_endline
+    | Error (Parse_error reason) -> eprintln "parse error: %s" reason
+    | Error (Runtime_error reason) -> eprintln "runtime error: %s" reason
     | _ -> ());
     repl ()
   with End_of_file -> ()
+
+let read_file_to_string path =
+  try Ok (In_channel.with_open_text path In_channel.input_all)
+  with Sys_error reason -> Error reason
+
+let run_file path =
+  let source =
+    read_file_to_string path
+    |> Result.get_or_else @@ fun reason ->
+       eprintln "failed to read file: %s" reason;
+       exit 1
+  in
+  match interpret source with
+  | Ok (Some expr) -> Runtime.Value.show expr |> print_endline
+  | Error (Parse_error reason) -> eprintln "parse error: %s" reason
+  | Error (Runtime_error reason) -> eprintln "runtime error: %s" reason
+  | _ -> ()
 
 let () = repl ()
